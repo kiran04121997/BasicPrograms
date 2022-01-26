3a7373d391dba355651d4c9162e6a32905d74908
kiran04121997 <bhosalekiran259@gmail.com>
2022-01-27 00:02:32 +0530

    welcometobasicprograms

---
+++ BasicCorePrograms/BasicCorePrograms/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicCorePrograms
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("welcome to basic core programs");
+            Console.ReadLine();
+        }
+    }
+}
