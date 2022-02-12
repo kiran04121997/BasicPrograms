aee8b6d94b22c004919b06550fcbe62a26445182
Kiran Bhosale <91077158+kiran04121997@users.noreply.github.com>
2022-02-12 17:20:01 +0530

    updated

--- BasicCorePrograms/BasicCorePrograms/HeadTailcs.cs
+++ BasicCorePrograms/BasicCorePrograms/HeadTailcs.cs
@@ -10,23 +10,16 @@ namespace BasicCorePrograms
     {
         public void Check_leap_year()
         {
-           Console.WriteLine("Please Enter a Year in four digits");
+           Console.WriteLine("Enter the year in 4 digit");
             int year = Convert.ToInt32(Console.ReadLine());
-            if (year >= 1000 && year <= 9999)
+            if ((year % 400 == 0) || (year % 4 == 0 && year % 100 != 0))
             {
-
-                if ((year % 400 == 0) || (year % 4 == 0 && year % 100 != 0))
-                {
-                    Console.WriteLine(year + " is a Leap year");
-
-                }
-                else
-                {
-                    Console.WriteLine(year + " is not a Leap year");
-                }
+                Console.WriteLine(year + "is a Leap Year");
             }
             else
-                Console.WriteLine("Please Enter four digit number");
+            {
+                Console.WriteLine(year + "is not a Leap Year");
+            }
         }
 
     }
