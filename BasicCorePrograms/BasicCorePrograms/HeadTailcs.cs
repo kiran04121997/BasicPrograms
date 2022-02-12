2fda32ad53d55b85159a33cd29caa5d6afa5acee
Kiran Bhosale <91077158+kiran04121997@users.noreply.github.com>
2022-02-12 17:23:41 +0530

    updated

--- BasicCorePrograms/BasicCorePrograms/HeadTailcs.cs
+++ BasicCorePrograms/BasicCorePrograms/HeadTailcs.cs
@@ -14,11 +14,11 @@ namespace BasicCorePrograms
             int year = Convert.ToInt32(Console.ReadLine());
             if ((year % 400 == 0) || (year % 4 == 0 && year % 100 != 0))
             {
-                Console.WriteLine(year + "is a Leap Year");
+                Console.WriteLine(year + " is a Leap Year");
             }
             else
             {
-                Console.WriteLine(year + "is not a Leap Year");
+                Console.WriteLine(year + " is not a Leap Year");
             }
         }
 
